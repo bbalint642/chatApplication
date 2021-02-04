90b48e5c8723ecc9a8ea55f9bf01b06609f40118
Balázs Bálint Balázs <b.balint642@gmail.com>
2021-02-04 12:57:11 +0100

    ChatFeed.jsx finally fixed
    There were typos in line 16, 40 and 42. This part of the app works fine now

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -13,7 +13,7 @@ const ChatFeed = (props) => {
         return keys.map((key, index) => {
             const message = messages[key];
             const lastMessageKey = index === 0 ? null : keys[index - 1];
-            const isMyMessage = userName === message.sender.userName;
+            const isMyMessage = userName === message.sender.username;
 
             return(
                 <div key= {`msg_${index}`} style={{ width: '100%'}}>
@@ -32,19 +32,20 @@ const ChatFeed = (props) => {
         })       
     }
 
-    
+    if(!chat) return 'Loading ...';
+
     return (
         <div className="chat-feed">
             <div className="chat-title-container">
-                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-title">{chat.title}</div>
                 <div className="chat-subtitle">
-                    {chat.people.map((person) => ` ${person.person.userName}`)}
+                    {chat.people.map((person) => ` ${person.person.username}`)}
                 </div>
             </div>
             {renderMessages()}
-            <div style={{ height: '100px'}} />
+            <div style={{ height: '100px' }} />
             <div className="message-form-container">
-                <MessageForm {...props} chatID={activeChat} />
+                <MessageForm { ...props} chatID={activeChat} />
             </div>            
         </div>
     );
