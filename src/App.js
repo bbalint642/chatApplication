b2dc688b80db97e49b74cd0ba2790e75f524976c
Balázs Bálint Balázs <b.balint642@gmail.com>
2021-02-07 15:10:52 +0100

    App.js FIX
    Fixed a bug where the browser just couldnt connect us to the chat engine, was caused by typos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { ChatEngine} from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
-
-
 import './App.css';
 
+const projectID = 'bfa47fe3-37e5-4fc6-bff0-be2a1da85341';
+
 const App = () => {
+    
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
             height="100vh"
-            projectID="bfa47fe3-37e5-4fc6-bff0-be2a1da85341"
-            userName="bbalint642"
-            userSecret="admin1234"
-            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} />}
         />
     );
-}
+};
 
 export default App;
